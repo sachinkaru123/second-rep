763fc386e564985ceb61608347e8e156ecf8cdbd
sachin <sachinkaru.tm@gmail.com>
2022-10-18 13:15:02 +0530

    h2 tag added

---
+++ first.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1 {
+            text-align: center;
+            margin-top: 100px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>This is a project</h1>
+    <!-- mkdir (-make directory) -->
+    <!-- git status (-make directory) reoostory ekk hadilada balala nattnm na kiyanawa -->
+    <!-- git init (meeken wenne git eken trac wenna opni file ekak kiyala pennanawa) -->
+    <!-- now see .git folder this folder is hidden -->
+    <!-- git add folder.html add to (trac wenawa) repostory ekata dd una -->
+    <!-- git add --all (to add all files to resp)-->
+    <!-- staging env ekata dn okkoma awa -->
+    <!-- git commit -m "Discription" (meeka nikan save point ekak wage apita meekedi api kara[pu changes tika discription ekata ddannat puluwan])-->
+    <!-- git log (meeken wenne api commit karapu details tika enawa discription eka date/time wage) -->
+    <!-- git branch test    (test kiyala branch ekak haduna) -->
+    <!-- git branch     (tiyena branchs balanna saha dn inna eka balanna) -->
+    <!-- git checkout test  (test kiyala tiyena branch ekata yanawa) -->
+    <!-- git branch -d test1    (aluth branch ekak create wela e branch eka select karanawa) -->
+    <!-- git branch -d test1        (deleate test1 branch) -->
+     <!-- apita iitapasse main ekata test branch eka merge karaganna oninm apita main branch ekata gihin 
+        git merge test (kiyala denna wenaw)-->
+    <!--  -->
+</body>
+
+</html>
